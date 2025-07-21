623f336358ac9364ea02c8925ba8ba9c610205f1
Muhammad Ahtisham Arif <ahtisham0100@gmail.com>
2025-07-21 15:13:05 +0500

    fixed dialog close btn

--- components/projects.tsx
+++ components/projects.tsx
@@ -98,14 +98,8 @@ const Projects = () => {
                       />
                     </div>
                     <DialogClose asChild>
-                      <Button
-                        variant="ghost"
-                        size="icon"
-                        className="absolute top-4 right-4 bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-600 rounded-full"
-                        aria-label="Close image preview"
-                      >
+                  
                         &times;
-                      </Button>
                     </DialogClose>
                   </DialogContent>
                 </Dialog>
